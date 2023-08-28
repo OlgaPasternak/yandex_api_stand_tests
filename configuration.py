b7ea7d798b7066fa9e596148a63f4867eed49a97
Olga_Pasternak <dr19teou@gmail.com>
2023-08-28 11:19:07 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://d30af393-3c5b-4f5b-ba99-b0a96446e8d2.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
